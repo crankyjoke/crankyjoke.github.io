34aae95b1023299f348da0d20c42119bc161cb62
Crankyjoke <100891466+crankyjoke@users.noreply.github.com>
2025-11-27 14:22:59 -0500

    1

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-logo">Crankyjoke</div>
+
+            <ul className="nav-links">
+                <li><a href="/about">About</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/courses">Courses</a> </li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Courses from "./pages/Courses"
+import Layout from "./Layout";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<App />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/courses" element={<Courses />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
